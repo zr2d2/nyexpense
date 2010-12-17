4918a1e3e9d4433086fd064c4d5836bc7ba285c9
zrowe007 <zrowe007@746b6086-5916-c7eb-dae2-5b39acbafa2d>
2010-12-17 02:15:30 +0000

    cleaned up feeds, labeks

--- trunk/nyt.php
+++ trunk/nyt.php
@@ -7,6 +7,7 @@
       $geocode=file_get_contents('http://api.nytimes.com/svc/search/v1/article?query=senate&api-key=13a26276609744cc626e399a4ec39753:11:61727267');
       $output= json_decode($geocode);
     }
+    echo '<h2>Related Stories from the New York Times</h2>';
     for ( $counter = 0; $counter <= 5; $counter += 1) {
       echo '<b> <a href="' . $output->results[$counter]->url . '" target="_blank">' . $output->results[$counter]->title . '<a/> (' . date('F jS, Y', strtotime($output->results[$counter]->date)) . ')</b><br>';
       echo $output->results[$counter]->body . '... <a href="' . $output->results[$counter]->url . '" target="_blank">read more</a><br><br>';
